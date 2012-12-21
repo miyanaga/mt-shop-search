c7421acccf8820e325f12508a2a4bb37216aa022
Kunihiko Miyanaga <miyanaga@ideamans.com>
2012-12-21 17:39:51 +0900

    Translated. List properties. Fix screens.

--- plugins/ShopSearch/lib/MT/ShopSearch/L10N/ja.pm
+++ plugins/ShopSearch/lib/MT/ShopSearch/L10N/ja.pm
@@ -9,14 +9,37 @@ use vars qw( %Lexicon );
     'Shop Search' => '店舗検索',
     'Shop' => '店舗',
     'Shops' => '店舗',
+    'Shop Name' => '店舗名',
     'Prefecture' => '都道府県',
     'Prefectures' => '都道府県',
     'Brand' => 'ブランド',
     'Brands' => 'ブランド',
+    'Tenant' => 'テナント',
+    'Tenants' => 'テナント',
     'Map Address' => '地図用住所',
     'Full Address' => '住所',
 
     'Shop Search Result' => '店舗検索結果',
+
+    'Sort Masters' => 'マスタの並び替え',
+
+    'Update Shops' => '店舗の更新',
+    'Copy all in sheet of Excel and paste here.' => 'エクセルのシートで全体をコピーし、ここに貼り付けてください。',
+    'TSV(Tab Separated Values)' => 'TSV(タブ区切りデータ)',
+    'Start Update' => '更新開始',
+
+    'Updating Shops' => '店舗の更新中',
+    'Updating now...' => '更新中です...',
+
+    'Updating Finished' => '更新完了',
+    'Shops updated from [_1] to [_2] shop(s).' => '店舗情報が[_1]件から、[_2]件に更新されました。',
+    'List Shops' => '店舗の一覧',
+
+    'Map' => '地図',
+    'Priority' => '優先度',
+    'Line Number' => '行番号',
+    'Comment' => '注記',
+    'Search Keyword' => '検索キーワード',
 );
 
 1;
